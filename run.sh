56e3f7925db2b6c1ed73281da9603d5f775391c3
0xFugue <119708655+0xFugue@users.noreply.github.com>
2023-01-22 13:13:53 +0530

    added run.sh: performs full waku runs for both lightpush/filter & pub/sub

--- run.sh
+++ run.sh
@@ -3,32 +3,49 @@
 cool_off_time=120
 sleep_time=$cool_off_time
 prefix=$1
+time="$(\date +"%Y-%m-%d-%Hh%Mm%Ss")"
 
 run(){
   duration=$1
-  echo $(date), "starting for $duration, with output at $prefix"
+  echo $time, "starting $duration run with output at $prefix"
   START=$(date +%s)
 
   sleep $sleep_time
-  ./run-waku.sh metal       $duration >> $prefix-$duration 2>&1
+  ./run-waku.sh metal       $duration $time lf >> $prefix-$duration-$time 2>&1
   sleep $sleep_time
-  ./run-waku.sh  docker     $duration >> $prefix-$duration 2>&1
+  ./run-waku.sh  docker     $duration $time lf >> $prefix-$duration-$time 2>&1
   sleep $sleep_time
-  ./run-waku.sh  kurtosis   $duration >> $prefix-$duration 2>&1
+  ./run-waku.sh  kurtosis   $duration $time lf >> $prefix-$duration-$time 2>&1
+
+  STOP=$(date +%s)
+  DIFF=$(( $STOP - $START ))
+  echo $time, "lf: $duration done"
+
+  echo "LF: $duration took $DIFF secs; metal, docker, kurtosis; sleep_time=$sleep_time)"  >> $prefix-$duration-$time 2>&1
+  echo "LF: $duration took $DIFF secs; metal, docker, kurtosis; sleep_time=$sleep_time)"
+
+  sleep $sleep_time
+  ./run-waku.sh metal       $duration $time ps >> $prefix-$duration-$time 2>&1
+  sleep $sleep_time
+  ./run-waku.sh  docker     $duration $time ps >> $prefix-$duration-$time 2>&1
+  sleep $sleep_time
+  ./run-waku.sh  kurtosis   $duration $time ps >> $prefix-$duration-$time 2>&1
 
 
   STOP=$(date +%s)
   DIFF=$(( $STOP - $START ))
-  echo $(date), "$duration done"
+  echo $time, "ps: $duration done"
 
-  echo "$duration took $DIFF secs; metal, docker, kurtosis; sleep_time=$sleep_time)"  >> $1-$duration 2>&1
-  echo "$duration took $DIFF secs; metal, docker, kurtosis; sleep_time=$sleep_time)"
+  echo "LF+PS: $duration took $DIFF secs; lf + ps; metal, docker, kurtosis; sleep_time=$sleep_time)"  >> $prefix-$duration-$time 2>&1
+  echo "LF+PS: $duration took $DIFF secs; lf + ps; metal, docker, kurtosis; sleep_time=$sleep_time)"
 }
 
-echo "HERE WO GO!"
+
+echo "$time: HERE WO GO!"
 run 32s
 run 64s
 run 128s
 run 256s
 run 512s
 run 1024s
+run 2048s
